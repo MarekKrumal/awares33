4baed7023f2c5d760c7d55a91e316265257eec82
BARONYXZ <krumalmarek@gmail.com>
2024-11-01 01:11:24 +0100

    React Query cache mutation (useMutation, setQueriesData vs invalidateQueries)

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -4,6 +4,8 @@ import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createPostSchema } from "@/lib/validation";
 
+import { postDataInclude } from "@/lib/types";
+
 export async function submitPost(input: string) {
   const { user } = await validateRequest(); //zkontrolujeme jestli user je authetifikovan
 
@@ -12,10 +14,13 @@ export async function submitPost(input: string) {
   const { content } = createPostSchema.parse({ content: input }); //tedka input neni empty protoze, v validations ma createSchema-requiredString
 
   //new entry do dabaze
-  await prisma.post.create({
+  const NewPost = await prisma.post.create({
     data: {
       content,
       userId: user.id,
     },
+    include: postDataInclude,
   });
+
+  return NewPost;
 }
